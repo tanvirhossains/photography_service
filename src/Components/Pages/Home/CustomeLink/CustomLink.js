0ecf377a5283b602749ba9f6fd7e8160f520e7ce
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-04-23 10:23:49 +0600

    log out

--- src/Components/Pages/Home/CustomeLink/CustomLink.js
+++ src/Components/Pages/Home/CustomeLink/CustomLink.js
@@ -10,13 +10,13 @@ function CustomLink({ children, to, ...props }) {
     return (
         <div>
             <Link
-                style={{ textDecoration: match ? "underline" : "none" }}
+                style={{ textDecoration: match ? "underline " : "none"  , color:match? 'red': ''}}
                 to={to}
                 {...props}
             >
                 {children}
             </Link>
-            {match && " (active)"}
+            {/* {match } */}
         </div>
     );
 }
--- src/Components/Pages/Home/Header/Header.js
+++ src/Components/Pages/Home/Header/Header.js
@@ -1,16 +1,29 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../../Firebase.init';
 import CustomLink from '../CustomeLink/CustomLink';
 import './Header.css'
 
 const Header = () => {
+      const [user] = useAuthState(auth)
+
+    const handleSignOut = () => {
+        signOut(auth)
+    }
     return (
         <div className='d-flex header'>
             <CustomLink to='/'>Home</CustomLink>
-        <CustomLink to='/services'>Services</CustomLink>
+            <CustomLink to='/services'>Services</CustomLink>
             <CustomLink to='/aboutme'>About Me</CustomLink>
             <CustomLink to='/blogs'>Blogs</CustomLink>
-            <CustomLink to='/login'>Login</CustomLink>
-            
+            {user ? <a href='' onClick={handleSignOut}>SignOut</a>
+                :
+                 <CustomLink to='/login'>Login</CustomLink>
+
+            }
+
+
         </div>
     );
 };
